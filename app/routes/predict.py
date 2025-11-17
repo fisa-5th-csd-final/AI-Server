9349bfeea924a91deaf4523952277353f44d225a
ehgkals <ehgkals12@gmail.com>
2025-11-17 10:29:41 +0900

    [FIX] user_id str->int로 수정

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -10,7 +10,7 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-def get_latest_features(db: Session, user_id: str):
+def get_latest_features(db: Session, user_id: int):
     query = text("""
         SELECT
             TOT_USE_AM_mean,
@@ -82,7 +82,7 @@ def get_latest_features(db: Session, user_id: str):
 
 
 @router.get("/predict/{user_id}", response_model=PredictResponse)
-def predict(user_id: str):
+def predict(user_id: int):
     try:
         db = FeatureSessionLocal()
 
